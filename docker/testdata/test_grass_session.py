decbe5b3933b7bae43d4a1438cda6ae38423bf51
Alex Barreto <axbarreto@lavabit.com>
2021-06-07 09:48:39 -0400

    Dockerfile: fix broken lib link (#1625)
    * Dockerfile: fix broken lib link

--- docker/testdata/test_grass_session.py
+++ docker/testdata/test_grass_session.py
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 # Import GRASS GIS Python bindings (requires 8.4+) and test r.in.pdal
 
 # PYTHONPATH=$(grass --config python-path) python
+=======
+# Import GRASS Python bindings
+# https://github.com/zarch/grass-session
+<<<<<<< HEAD
+# pip install grass-session
+=======
+>>>>>>> 756514063b (Dockerfile: fix broken lib link (#1625))
+>>>>>>> 92e9b42dca (Dockerfile: fix broken lib link (#1625))
 
 import grass.script as gs
 
