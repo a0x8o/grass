4e0c4a2ccadd376656615aa8cc86d20bb852890e
Alex Barreto <axbarreto@lavabit.com>
2021-07-12 09:39:22 -0400

    libpython: Save and load benchmark results (#1711)
    Series of functions to save benchmark results to JSON, load them back, and combine them together.
    
    This needs some command line interface to make it convenient, but I used it in a custom benchmark script.
    
    The JSON save ensures that the saved results are a dictionary not just a list which makes the storage format more extensible as first level can contain additional keys.

--- python/grass/benchmark/__init__.py
+++ python/grass/benchmark/__init__.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # MODULE:    grass.benchmark
 #
 # AUTHOR(S): Vaclav Petras <wenzeslaus gmail com>
@@ -10,11 +11,14 @@
 #            License (>=v2). Read the file COPYING that comes with GRASS
 #            for details.
 
+=======
+>>>>>>> da7f79c3f9 (libpython: Save and load benchmark results (#1711))
 """Benchmarking for GRASS GIS modules
 
 This subpackage of the grass package is experimental and the API can change anytime.
 The API of the package is defined by what is imported in the top-level ``__init__.py``
 file of the subpackage.
+<<<<<<< HEAD
 
 The functions in the Python API raise exceptions, although calls of other functions from
 the grass package may call grass.script.fatal and exit
@@ -22,18 +26,25 @@ the grass package may call grass.script.fatal and exit
 This applies to the CLI interface of this subpackage too except that raised usage
 exceptions originating in the CLI code result in *sys.exit* with an error message, not
 traceback. Messages and other user-visible texts in this package are not translatable.
+=======
+>>>>>>> da7f79c3f9 (libpython: Save and load benchmark results (#1711))
 """
 
 from .plots import nprocs_plot, num_cells_plot
 from .results import (
     join_results,
+<<<<<<< HEAD
     join_results_from_files,
+=======
+>>>>>>> da7f79c3f9 (libpython: Save and load benchmark results (#1711))
     load_results,
     load_results_from_file,
     save_results,
     save_results_to_file,
 )
+<<<<<<< HEAD
 from .runners import benchmark_nprocs, benchmark_resolutions, benchmark_single
+<<<<<<< HEAD
 
 __all__ = [
     "benchmark_nprocs",
@@ -48,3 +59,8 @@ __all__ = [
     "save_results",
     "save_results_to_file",
 ]
+=======
+=======
+from .runners import benchmark_nprocs, benchmark_resolutions
+>>>>>>> da7f79c3f9 (libpython: Save and load benchmark results (#1711))
+>>>>>>> f0a06841f5 (libpython: Save and load benchmark results (#1711))
