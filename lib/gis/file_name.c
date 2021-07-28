19f46cf73280dac6d805ded134f1ca4141f7e6ef
Alex Barreto <axbarreto@lavabit.com>
2021-07-28 09:15:29 -0400

    Improve G_open|find _misc function documentation (#1760)
    Lib GIS: improve documentation for G_file_name_misc, G_find_file_misc and G_fopen_(old|new)_misc

--- lib/gis/file_name.c
+++ lib/gis/file_name.c
@@ -177,6 +177,7 @@ static void append_char(char *, char);
   If <i>name</i> is of the form "nnn@ppp" then path is set as if name
   had been "nnn" and mapset had been "ppp" (mapset parameter itself is
   ignored in this case).
+<<<<<<< HEAD
 =======
    If <i>name</i> is of the form "nnn@ppp" then path is set as if name
    had been "nnn" and mapset had been "ppp" (mapset parameter itself is
@@ -206,6 +207,7 @@ static void append_char(char *, char);
    >>> /full/path/to/mapset/elem/name
    @endcode
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -409,6 +411,34 @@ static void append_char(char *, char);
 >>>>>>> osgeo-main
 =======
 >>>>>>> osgeo-main
+=======
+=======
+
+  Paths to files are currently in form:
+  /path/to/location/mapset/element/name
+
+  path input buffer memory must be allocated by caller.
+
+  C:
+  @code{.c}
+  char path[GPATH_MAX];
+  G_file_name(path, "fcell", "my_raster", "my_mapset");
+  // path now is "/full/path/to/my_mapset/fcell/my_raster"
+  @endcode
+  Python:
+  @code{.py}
+  import ctypes
+  from grass.pygrass.utils import decode
+  from grass.lib.gis import G_file_name, GPATH_MAX
+
+  path = ctypes.create_string_buffer(GPATH_MAX)
+  path_str = decode(G_file_name(path, "elem", "name", "mapset"))
+  print(path_str)
+  >>> /full/path/to/mapset/elem/name
+  @endcode
+
+>>>>>>> 6dd43833fd (Improve G_open|find _misc function documentation (#1760))
+>>>>>>> 86cf92e040 (Improve G_open|find _misc function documentation (#1760))
   \param[out] path allocated buffer to hold resultant full path to file
   \param element database element (eg, "cell", "cellhd", "vector", etc)
   \param name name of file to build path to (fully qualified names allowed)
@@ -831,8 +861,11 @@ char *G_file_name(char *path, const char *element, const char *name,
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> main
+=======
+>>>>>>> 86cf92e040 (Improve G_open|find _misc function documentation (#1760))
    Paths to misc files are currently in form:
    /path/to/location/mapset/dir/name/element
 =======
@@ -932,6 +965,7 @@ char *G_file_name(char *path, const char *element, const char *name,
    >>> /full/path/to/mapset/dir/name/elem
    @endcode
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -1157,6 +1191,33 @@ char *G_file_name(char *path, const char *element, const char *name,
 >>>>>>> osgeo-main
 =======
 >>>>>>> osgeo-main
+=======
+=======
+  Paths to misc files are currently in form:
+  /path/to/location/mapset/dir/name/element
+
+  path input buffer memory must be allocated by caller.
+
+  C:
+  @code{.c}
+  char path[GPATH_MAX];
+  G_file_name_misc(path, "cell_misc", "history", "my_raster", "my_mapset");
+  // path now contains "/full/path/to/my_mapset/cell_misc/my_raster/history"
+  @endcode
+  Python:
+  @code{.py}
+  import ctypes
+  from grass.pygrass.utils import decode
+  from grass.lib.gis import G_file_name_misc, GPATH_MAX
+
+  path = ctypes.create_string_buffer(GPATH_MAX)
+  path_str = decode(G_file_name_misc(path, "dir", "elem", "name", "mapset"))
+  print(path_str)
+  >>> /full/path/to/mapset/dir/name/elem
+  @endcode
+
+>>>>>>> 6dd43833fd (Improve G_open|find _misc function documentation (#1760))
+>>>>>>> 86cf92e040 (Improve G_open|find _misc function documentation (#1760))
   \param[out] path allocated buffer to hold resultant full path to file
   \param dir misc directory (e.g., "cell_misc", "group")
   \param element database element (in this case – file to build path to e.g., "history", "REF")
