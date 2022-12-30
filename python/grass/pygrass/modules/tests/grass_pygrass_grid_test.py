81d97e00cbf5cb2dc75b2cde8578a27c1ccee12b
Alexandre Barreto <abarre25@jh.edu>
2022-12-30 16:42:32 -0500

    wxpyimgview: explicit conversion to int (#2704)

--- python/grass/pygrass/modules/tests/grass_pygrass_grid_test.py
+++ python/grass/pygrass/modules/tests/grass_pygrass_grid_test.py
@@ -232,6 +232,7 @@ def test_tiling(tmp_path, width, height, processes):
         info = gs.raster_info("slope")
         assert info["min"] > 0
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 
 @pytest.mark.needs_solo_run
@@ -270,3 +271,5 @@ def test_patching_error(tmp_path, processes, backend):
         assert int(info["null_cells"]) == 0
 =======
 >>>>>>> 6cf60c76a4 (wxpyimgview: explicit conversion to int (#2704))
+=======
+>>>>>>> 8422103f4c (wxpyimgview: explicit conversion to int (#2704))
