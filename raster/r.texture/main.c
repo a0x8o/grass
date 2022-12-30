3309d04075531bc70070f217e70f687011b5f8ec
Alexandre Barreto <abarre25@jh.edu>
2022-12-30 16:41:48 -0500

    wxpyimgview: explicit conversion to int (#2704)

--- raster/r.texture/main.c
+++ raster/r.texture/main.c
@@ -312,6 +312,7 @@ int main(int argc, char *argv[])
     out_set.flag_null = flag.null;
     out_set.flag_ind = flag.ind;
 
+<<<<<<< HEAD
     threads = atoi(parm.nproc->answer);
 #if defined(_OPENMP)
     /* Set the number of threads */
@@ -326,6 +327,24 @@ int main(int argc, char *argv[])
     }
 #endif
     execute_texture(data, &dim, measure_menu, measure_idx, &out_set, threads);
+=======
+<<<<<<< HEAD
+    execute_texture(data, &dim, measure_menu, measure_idx, &out_set);
+=======
+    /* *************************************************************************************************
+     *
+     * Compute of the matrix S.G.L.D. (Spatial Gray-Level Dependence Matrices)
+<<<<<<< HEAD
+     *or co-occurrence matrix. The image is analyzed for piece, every piece is
+=======
+     *or co-occurrence matrix. The image is analized for piece, every piece is
+>>>>>>> 6cf60c76a4 (wxpyimgview: explicit conversion to int (#2704))
+     *naming moving window (s.w.). The s.w. must be square with number of size's
+     *samples odd, that because we want the sample at the center of matrix.
+     *
+     ***************************************************************************************************/
+>>>>>>> 12b43eb397 (wxpyimgview: explicit conversion to int (#2704))
+>>>>>>> ebc6d3f683 (wxpyimgview: explicit conversion to int (#2704))
 
     for (i = 0; i < dim.n_outputs; i++) {
         Rast_close(outfd[i]);
