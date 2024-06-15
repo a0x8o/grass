63cba58c022ea403f5f91954447067ca8a42e9ef
Markus Neteler <neteler@osgeo.org>
2024-06-15 14:12:03 +0200

    CI: replace --tmp-location with --tmp-project (#3825)

--- .github/workflows/test_thorough.bat
+++ .github/workflows/test_thorough.bat
@@ -1,5 +1,10 @@
 set grass=%1
 set python=%2
 
+<<<<<<< HEAD
 call %grass% --tmp-project XY --exec g.download.project url=https://grass.osgeo.org/sampledata/north_carolina/nc_spm_full_v2alpha2.tar.gz path=%USERPROFILE%
 call %grass% --tmp-project XY --exec %python% -m grass.gunittest.main --grassdata %USERPROFILE% --location nc_spm_full_v2alpha2 --location-type nc --min-success 96 --config .github\workflows\osgeo4w_gunittest.cfg
+=======
+call %grass% --tmp-project XY --exec g.download.location url=https://grass.osgeo.org/sampledata/north_carolina/nc_spm_full_v2alpha2.tar.gz path=%USERPROFILE%
+call %grass% --tmp-project XY --exec %python% -m grass.gunittest.main --grassdata %USERPROFILE% --location nc_spm_full_v2alpha2 --location-type nc --min-success 80
+>>>>>>> 4c8d884044 (CI: replace --tmp-location with --tmp-project (#3825))
--- .github/workflows/test_thorough.sh
+++ .github/workflows/test_thorough.sh
@@ -4,7 +4,11 @@
 set -e
 
 grass --tmp-project XY --exec \
+<<<<<<< HEAD
     g.download.project url=https://grass.osgeo.org/sampledata/north_carolina/nc_spm_full_v2alpha2.tar.gz path=$HOME
+=======
+    g.download.location url=https://grass.osgeo.org/sampledata/north_carolina/nc_spm_full_v2alpha2.tar.gz path=$HOME
+>>>>>>> 4c8d884044 (CI: replace --tmp-location with --tmp-project (#3825))
 
 grass --tmp-project XY --exec \
     python3 -m grass.gunittest.main \
