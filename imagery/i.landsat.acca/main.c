ab51beb8cc6a735ca366c905ba05e21b8651c442
ShubhamDesai <42180509+ShubhamDesai@users.noreply.github.com>
2024-07-01 02:50:34 -0400

    i.landsat.acca: Fix uninitialized variable issue (#3958)
    Co-authored-by: Shubham Vasudeo Desai <sdesai8@vclvm179-48.vcl.ncsu.edu>

--- imagery/i.landsat.acca/main.c
+++ imagery/i.landsat.acca/main.c
@@ -178,11 +178,15 @@ int main(int argc, char *argv[])
     for (i = BAND2; i <= BAND6; i++) {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         band[i].name[0] = '\0';
 =======
 >>>>>>> 6cf60c76a4 (wxpyimgview: explicit conversion to int (#2704))
 =======
 >>>>>>> 8422103f4c (wxpyimgview: explicit conversion to int (#2704))
+=======
+        band[i].name[0] = '\0';
+>>>>>>> f720eb618b (i.landsat.acca: Fix uninitialized variable issue (#3958))
         sprintf(band[i].name, "%s%d%c", in_name, i + 2,
                 (i == BAND6 && !sat5->answer ? '1' : '\0'));
         band[i].fd = check_raster(band[i].name);
