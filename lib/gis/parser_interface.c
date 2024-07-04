28514e3c6dd59de381fe57de408f6efae8023b9f
ShubhamDesai <42180509+ShubhamDesai@users.noreply.github.com>
2024-07-04 12:40:46 -0400

    lib: Fix uninitialized variable 'encoding' in parser_interface.c (#3975)
    Fix uninitialized variable 'encoding' in parser_interface.c
    
    Co-authored-by: Shubham Vasudeo Desai <sdesai8@vclvm178-253.vcl.ncsu.edu>

--- lib/gis/parser_interface.c
+++ lib/gis/parser_interface.c
@@ -269,7 +269,7 @@ void G__usage_xml(void)
     char *type;
     char *s, *top;
     int i;
-    const char *encoding;
+    const char *encoding = NULL;
     int new_prompt = 0;
 
     new_prompt = G__uses_new_gisprompt();
