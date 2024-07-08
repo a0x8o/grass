55f0aeaf3dd3de0eaf9ca49edcc5bfe82b268672
ShubhamDesai <42180509+ShubhamDesai@users.noreply.github.com>
2024-07-08 07:17:08 -0400

    lib: Fixed uninitialized variable warning in dmax.c by initializing dp, dp_max, dm, and dm_max to 0.0 (#3995)
    Fixed uninitialized variable warning in dmax.c by initializing dp, dp_max, dm, and dm_max to 0.0.

--- lib/cdhc/dmax.c
+++ lib/cdhc/dmax.c
@@ -9,12 +9,16 @@ double *Cdhc_dmax(double *x, int n)
     double *xcopy, sqrt2, mean = 0.0, sdx = 0.0, fx;
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     double dp = 0.0, dp_max = 0.0, dm = 0.0, dm_max = 0.0;
 =======
 =======
 >>>>>>> 8422103f4c (wxpyimgview: explicit conversion to int (#2704))
     double dp, dp_max, dm, dm_max;
 >>>>>>> 6cf60c76a4 (wxpyimgview: explicit conversion to int (#2704))
+=======
+    double dp = 0.0, dp_max = 0.0, dm = 0.0, dm_max = 0.0;
+>>>>>>> a0dacc4619 (lib: Fixed uninitialized variable warning in dmax.c by initializing dp, dp_max, dm, and dm_max to 0.0 (#3995))
     int i;
 
     if ((xcopy = (double *)malloc(n * sizeof(double))) == NULL) {
